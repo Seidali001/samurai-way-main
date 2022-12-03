c1d1ac33c42a048e3d078ce68dd2ca240c0b5061
Seidali_ <seitmambetovseidali@gmail.com>
2022-12-03 19:34:30 +0000

    add-logo-to-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import './logo-hivar.png'
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App app-wrapper">
+            <header>
+                <img src="logo-hivar.png" alt="logo-hivar"/>
+            </header>
+            <nav>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Message
+                </div>
+                <div>
+                    Main content
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
