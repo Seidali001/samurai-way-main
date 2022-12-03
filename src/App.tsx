57bd8e76e1ee6b13fabcea1615c6e0df8416f54b
Seidali_ <seitmambetovseidali@gmail.com>
2022-12-03 23:36:36 +0000

    add-contentIMG-posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import hivar from './hivar.png'
+import world from './world.jpg'
 
 
 function App() {
     return (
-        <div className="App app-wrapper">
+        <div className="app-wrapper">
             <div className="header">
                 <header>
                     <img
@@ -15,15 +16,34 @@ function App() {
             </div>
             <nav className="nav">
                 <div>
-                    Profile
+                    <a>Profile</a>
                 </div>
                 <div>
-                    Message
+                    <a>Messages</a>
                 </div>
-                <div className="content">
-                    Main content
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Settings</a>
                 </div>
             </nav>
+            <div className="content">
+                <div>
+                    <img src={world}/>
+                </div>
+                <div>
+                    ava + discrip
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
         </div>
     );
 }
