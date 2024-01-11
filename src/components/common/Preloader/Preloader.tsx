29b3ab2013eb70bbbbb968203ee2aea01278505e
Seidali001 <seitmambetovseidali@gmail.com>
2024-01-11 00:20:13 +0000

    deleted unused one comments in Preloader component

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import preloader3 from "../../../assets/image/loader3.gif";
 import Skeleton from "@mui/material/Skeleton";
 import s from "../../../components/common/Preloader/Preloader.module.css"
 
@@ -7,7 +6,6 @@ const Preloader = () => {
     return (
         <div className={s.skletonContainer}>
              <span className={s.skeletonAvatar}>
-           {/* <img alt="preloader" src={preloader3}/>*/}
                  <Skeleton animation="wave" variant="circular" width={40} height={40}/>
             <span className={s.skeletonInfo}>
                 <Skeleton
